7014814546a35f7c963eb3774f1c540679753573
Matthieu Mota <matthieumota@gmail.com>
2019-04-27 17:01:27 +0200

    Create Boxydev Slide module

---
+++ app/code/Boxydev/Slider/registration.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the magento.com package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Boxydev_Slider',
+    __DIR__
+);
