d75b28d64d2fdd3598555ef61352fc81545925c0
Matthieu Mota <matthieumota@gmail.com>
2019-05-02 12:15:02 +0200

    Cart event observer

---
+++ app/code/Boxydev/Learn/Observer/CartObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the magento.com package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Boxydev\Learn\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class CartObserver implements ObserverInterface
+{
+    public function execute(Observer $observer)
+    {
+        return $observer;
+    }
+}
