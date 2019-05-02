edb0eea588aee837490b3cb0664a0ab2d573a86e
Matthieu Mota <matthieumota@gmail.com>
2019-05-02 19:11:42 +0200

    Source actions

--- app/code/Boxydev/Slider/Ui/Component/Listing/Column/SlideActions.php
+++ app/code/Boxydev/Slider/Ui/Component/Listing/Column/SlideActions.php
@@ -11,12 +11,52 @@
 
 namespace Boxydev\Slider\Ui\Component\Listing\Column;
 
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
 
 class SlideActions extends Column
 {
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
     public function prepareDataSource(array $dataSource)
     {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'boxydev/slide/edit',
+                        ['id' => $item['id']]
+                    ),
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+                $item[$this->getData('name')]['delete'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'boxydev/slide/delete',
+                        ['id' => $item['id']]
+                    ),
+                    'label' => __('Delete'),
+                    'hidden' => false,
+                ];
+            }
+        }
+
         return $dataSource;
     }
 }
