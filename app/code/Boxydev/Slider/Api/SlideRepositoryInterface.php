73cb11242448017fa7f06eab8468362f5e318e62
Matthieu Mota <matthieumota@gmail.com>
2019-05-07 11:29:30 +0200

    Repositories interfaces

---
+++ app/code/Boxydev/Slider/Api/SlideRepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the magento.com package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Boxydev\Slider\Api;
+
+use Boxydev\Slider\Api\Data\SlideInterface;
+use Magento\Framework\Api\SearchCriteriaInterface;
+
+interface SlideRepositoryInterface
+{
+    public function save(SlideInterface $slide);
+
+    public function getById($id);
+
+    public function getList(SearchCriteriaInterface $criteria);
+
+    public function delete(SlideInterface $slide);
+
+    public function deleteById($id);
+}
