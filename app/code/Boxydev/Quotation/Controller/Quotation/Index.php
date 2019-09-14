7f7cddb82a7463877ebcc8d4e7c5538997d2fff5
Matthieu Mota <matthieumota@gmail.com>
2019-09-14 11:03:18 +0200

    Validate form PHP

--- app/code/Boxydev/Quotation/Controller/Quotation/Index.php
+++ app/code/Boxydev/Quotation/Controller/Quotation/Index.php
@@ -13,6 +13,28 @@ class Index extends Action
         /** @var Page $page */
         $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
 
+        $request = $this->getRequest();
+
+        if ('POST' === $request->getMethod()) {
+            $name = $request->getParam('name');
+            $email = $request->getParam('email');
+            $hasError = false;
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $hasError = true;
+                $this->messageManager->addErrorMessage('Cet email n\'est pas valide.');
+            }
+
+            if (strlen($name) < 10) {
+                $hasError = true;
+                $this->messageManager->addErrorMessage('Votre nom est trop court.');
+            }
+
+            if (!$hasError) {
+                $this->messageManager->addSuccessMessage('Votre devis a bien été envoyé.');
+            }
+        }
+
         return $page;
     }
 }
