ff1aedeb1bb9781d7545d857ec5f53d6c13369d8
Matthieu Mota <matthieumota@gmail.com>
2019-09-29 08:55:02 +0200

    Boxydev shipping part

---
+++ app/code/Boxydev/Checkout/Model/Carrier/BoxydevShipping.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Boxydev\Checkout\Model\Carrier;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\ResultFactory;
+
+class BoxydevShipping extends AbstractCarrier implements CarrierInterface
+{
+    protected $_code = 'boxydevshipping';
+
+    /**
+     * @var ResultFactory
+     */
+    private $rateResultFactory;
+
+    /**
+     * @var MethodFactory
+     */
+    private $rateMethodFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
+        \Psr\Log\LoggerInterface $logger,
+        ResultFactory $rateResultFactory,
+        MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+
+        $this->rateResultFactory = $rateResultFactory;
+        $this->rateMethodFactory = $rateMethodFactory;
+    }
+
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigData('active')) {
+            return false;
+        }
+
+        /** @var Result $result */
+        $result = $this->rateResultFactory->create();
+
+        /** @var Method $method */
+        $method = $this->rateMethodFactory->create();
+
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod($this->_code);
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $shippingCost = (float) $this->getConfigData('shipping_cost');
+
+        $method->setPrice($shippingCost);
+        $method->setCost($shippingCost);
+
+        $result->append($method);
+
+        return $result;
+    }
+
+    public function getAllowedMethods()
+    {
+        return [
+            $this->_code => $this->getConfigData('name'),
+        ];
+    }
+}
