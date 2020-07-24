691a3fd66ca9e297cd0b93e79f8028c7ae488059
Matthieu Mota <matthieumota@gmail.com>
2020-07-24 23:14:25 +0200

    Optimize

--- app/code/Boxydev/Checkout/Model/Carrier/BoxydevShipping.php
+++ app/code/Boxydev/Checkout/Model/Carrier/BoxydevShipping.php
@@ -1,11 +1,23 @@
 <?php
 
+/*
+ * This file is part of the public package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Boxydev\Checkout\Model\Carrier;
 
+use Magento\Customer\Model\Session;
 use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Quote\Model\Quote\Address\RateResult\Method;
 use Magento\Quote\Model\Quote\Address\RateResult\MethodFactory;
 use Magento\Shipping\Model\Carrier\AbstractCarrier;
 use Magento\Shipping\Model\Carrier\CarrierInterface;
+use Magento\Shipping\Model\Rate\Result;
 use Magento\Shipping\Model\Rate\ResultFactory;
 
 class BoxydevShipping extends AbstractCarrier implements CarrierInterface
@@ -22,25 +34,42 @@ class BoxydevShipping extends AbstractCarrier implements CarrierInterface
      */
     private $rateMethodFactory;
 
+    /**
+     * @var Session
+     */
+    private $customerSession;
+
     public function __construct(
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
         \Psr\Log\LoggerInterface $logger,
         ResultFactory $rateResultFactory,
         MethodFactory $rateMethodFactory,
+        Session $customerSession,
         array $data = []
     ) {
         parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
 
         $this->rateResultFactory = $rateResultFactory;
         $this->rateMethodFactory = $rateMethodFactory;
+        $this->customerSession = $customerSession;
     }
 
+    /**
+     * Collect and get rates
+     *
+     * @param RateRequest $request
+     * @return \Magento\Framework\DataObject|bool|null
+     * @api
+     */
     public function collectRates(RateRequest $request)
     {
         if (!$this->getConfigData('active')) {
             return false;
         }
+
+        $shippingAddress = $this->customerSession->getCustomer()->getDefaultShippingAddress();
+        $postCode = (int) substr($request->getDestPostcode(), 0, 2);
 
         /** @var Result $result */
         $result = $this->rateResultFactory->create();
@@ -54,20 +83,38 @@ class BoxydevShipping extends AbstractCarrier implements CarrierInterface
         $method->setMethod($this->_code);
         $method->setMethodTitle($this->getConfigData('name'));
 
-        $shippingCost = (float) $this->getConfigData('shipping_cost');
+        // $shippingCost = (float) $this->getConfigData('shipping_cost');
+        $shippingCost = (float) $postCode * 2;
 
         $method->setPrice($shippingCost);
         $method->setCost($shippingCost);
 
         $result->append($method);
 
+        // Seconde méthode
+        $method = $this->rateMethodFactory->create();
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+        $method->setMethod($this->_code . '-2');
+        $method->setMethodTitle($this->getConfigData('name') . ' 2');
+        $method->setPrice($shippingCost * 2);
+        $method->setCost($shippingCost * 2);
+        $result->append($method);
+
         return $result;
     }
 
+    /**
+     * Get allowed shipping methods
+     *
+     * @return array
+     * @api
+     */
     public function getAllowedMethods()
     {
         return [
             $this->_code => $this->getConfigData('name'),
+            $this->_code . '-2' => $this->getConfigData('name') . ' 2',
         ];
     }
 }
--- app/code/Boxydev/Checkout/Model/Payment/Boxydev.php
+++ app/code/Boxydev/Checkout/Model/Payment/Boxydev.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the public package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Boxydev\Checkout\Model\Payment;
 
 use Magento\Payment\Model\InfoInterface;
@@ -16,13 +25,43 @@ class Boxydev extends Cc
 
     public function capture(InfoInterface $payment, $amount)
     {
+        // Instancier Stripe avec la clé API privée
+        // Idéalement, stocker la clé en BO
+        \Stripe\Stripe::setApiKey('sk_test_oVCtBcho5lERfuAKA57WUg5E');
+
+        // On doit "charger" le client avec son numéro de carte
+        try {
+            $customer = \Stripe\Customer::create([
+                'email' => $payment->getOrder()->getBillingAddress()->getEmail(),
+            ]);
+            $card = \Stripe\Customer::createSource($customer->id, [
+                'source' => [
+                    'object' => 'card',
+                    'number' => $payment->getCcNumber(),
+                    'exp_month' => $payment->getCcExpMonth(),
+                    'exp_year' => $payment->getCcExpYear(),
+                    'cvc' => $payment->getCcCid()
+                ]
+            ]);
+            $charge = \Stripe\Charge::create([
+                'amount' => $amount * 100,
+                'currency' => 'eur',
+                'customer' => $customer->id
+            ]);
+        } catch (\Exception $e) {
+            throw new \Magento\Framework\Validator\Exception(__($e->getMessage()));
+        }
+
         $this->debugData([
             'order' => $payment->getCcNumber(),
             'billing' => $payment->getOrder()->getBillingAddress()->getName(),
-            'amount' => $amount
+            'amount' => $amount,
+            'card' => $card,
         ]);
 
-        throw new \Magento\Framework\Validator\Exception(__('Payment capturing error.'));
+        // Changer le status de la commande et les informations de transactions
+
+        // throw new \Magento\Framework\Validator\Exception(__('Payment capturing error.'));
     }
 
     public function isAvailable(?CartInterface $quote = null)
